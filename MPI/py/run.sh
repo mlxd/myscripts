22a50c81e833c07710e1268d9bdf83dd7263321f
mlxd <loriordan@gmail.com>
2018-01-13 16:02:57 -0800

    safety checking for number input

--- MPI/py/run.sh
+++ MPI/py/run.sh
@@ -4,8 +4,15 @@
 # to create (allows us to oversubscribe MPI processes to cores)
 # the number of which are passed as an argument to ./run.sh
 echo "localhost slots=$1" > hostfile
-
+ranks=$1
 #Make sure mpi4py is installed before running
-mpirun --hostfile ./hostfile -n $1 python2 ./mpi_reduce_time.py > reduce_timing.dat
+which python2 &> /dev/null
+if ! [[ $? -eq 0 ]];then
+    echo "python2 not found. Please ensure it is installed and available on your path."
+fi
+if [[ $# -gt 0 && "${ranks}" -gt 1 ]];then
+    mpirun --hostfile ./hostfile -n ${ranks} python2 ./mpi_reduce_time.py > reduce_timing.dat
+    sh ./plot_mpi_timing.sh ./reduce_timing.dat
+else echo "Please provide the number of MPI ranks >2 ( e.g. ./run.sh 4 )"
+fi
 
-sh ./plot_mpi_timing.sh ./reduce_timing.dat
